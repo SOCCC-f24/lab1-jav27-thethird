ddaf77cd6d7191428e544253f4a74c1453667996
jav27-thethird <jv142@sunyorange.edu>
2024-12-10 18:04:01 -0500

    Update temp_converter.py
    added parentheses around the 5 / 9 part of the celsius to farenheit formula

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
 def f2c_raw(f): #IGNORE ME
     return f - 32 * 5 / 9
 def f2c_op(f):
-    return f - 32 * 5 / 9 #TODO edit me
+    return f - 32 * (5 / 9) #TODO edit me
+    #adding parentheses around the division as it is associative rather than commutitive - TEST
